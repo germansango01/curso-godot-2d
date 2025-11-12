db8fe04597ee7813e7d6a333a2745954964a306b
Germán Contreras <234036903+germansango01@users.noreply.github.com>
2025-11-13 00:23:19 +0100

    update game

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -1,43 +1,35 @@
 extends Node
 
 @export var mob_scene: PackedScene
-
 var score
 
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
-func _ready():
-	new_game()
 
-
-func new_game()-> void:
+func new_game():
+	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 
-func game_over() -> void:
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-
-
-func _on_start_timer_timeout() -> void:
-	$MobTimer.start()
-	$ScoreTimer.start()
-
-
-func _on_score_timer_timeout() -> void:
-	score += 1
-
-
-func _on_mob_timer_timeout() -> void:
+func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
 
 	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
+	var mob_spawn_location = get_node(^"MobPath/MobSpawnLocation")
+	mob_spawn_location.progress = randi()
 
-	# Set the mob's position to the random location.
+	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
 
 	# Set the mob's direction perpendicular to the path direction.
@@ -53,3 +45,13 @@ func _on_mob_timer_timeout() -> void:
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,15 +9,16 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed(&"move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed(&"move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed(&"move_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
@@ -30,21 +31,24 @@ func _process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size)
 
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.animation = &"right"
 		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
+		$Trail.rotation = 0
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = &"up"
+		rotation = PI if velocity.y > 0 else 0.0
 
-func _on_body_entered(_body: Node2D) -> void:
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos):
 	position = pos
+	rotation = 0
 	show()
 	$CollisionShape2D.disabled = false
+
+
+func _on_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred(&"disabled", true)
